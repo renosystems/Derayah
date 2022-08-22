fcbd3b5097737ae314219fdc9cc83ec30dba7d60
dohaShiref <50174692+dohaShiref@users.noreply.github.com>
2022-08-22 14:37:08 +0200

    update

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ function openLeftSection(sectionName, elmnt) {
     document.getElementById("mySidenav").style.width = "0";
   }
   
+  const accordionItemHeaders2 = document.querySelectorAll(
+    ".accordionn-item-headerr2"
+  );
+  accordionItemHeaders2.forEach((accordionItemHeader2) => {
+    accordionItemHeader2.addEventListener("click", (event) => {
+      accordionItemHeader2.classList.toggle("active");
+      const accordionItemBody2 = accordionItemHeader2.nextElementSibling;
+      if (accordionItemHeader2.classList.contains("active")) {
+        accordionItemBody2.style.maxHeight =
+          accordionItemBody2.scrollHeight + "px";
+      } else {
+        accordionItemBody2.style.maxHeight = 0;
+      }
+    });
+  });
+  
